27a0516dbfe1e042c4404db8946455df3b4181f3
jimkang83 <jim.kang83@gmail.com>
2022-12-15 17:46:11 -0700

    3.6 Middlewares

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+// const privateMiddleware = (req, res, next) => {
+//     const url = req.url;
+//     if(url === "/protected"){
+//         return res.send("<h1>Not Allowed</h1>");
+//     }
+//     console.log("Allowed, you may continue.");
+//     next();
+// }
+
+const handlehome = (req, res) => {
+    return res.send("I love middlewares");
+};
+
+app.get("/", logger, handlehome);
+
+// const handleProtected = (req, res) => {
+//     return res.send("Welcome to the private lounge.");
+// }
+
+// app.use(logger);
+// app.get("/protected", logger, handleProtected);
+
+const handleListening = () => 
+    console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
+
+
