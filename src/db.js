b7b36fcbb25865df43d20fc6d58a0ddde2ed7679
jimkang83 <jim.kang83@gmail.com>
2022-12-24 17:36:16 -0700

    6.8 Connetcting to Mongo

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {useNewUrlParser: true});
+
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log("DE Error", error);
+
+mongoose.connection.on("erro", handleError);
+mongoose.connection.once("open", handleOpen);
