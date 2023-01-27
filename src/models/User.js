b6511cc0a781e0482ea5d0751d8bf89872950373
jimkang83 <jim.kang83@gmail.com>
2023-01-27 02:05:51 -0700

    16.9 Comments

--- src/models/User.js
+++ src/models/User.js
@@ -2,13 +2,16 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, requried: true, unique: true },
   avatarUrl: String,
   socialOnly: { type: Boolean, default: false },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, requried: true, unique: true },
   password: { type: String },
-  name: { type: String, required: true },
+  name: { type: String, requried: true },
   location: String,
+  comment: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
+  ],
   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
@@ -20,26 +23,3 @@ userSchema.pre("save", async function () {
 
 const User = mongoose.model("User", userSchema);
 export default User;
-
-// import bcrypt from "bcrypt";
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema({
-//   email: { type: String, requried: true, unique: true },
-//   avatarUrl: String,
-//   socialOnly: { type: Boolean, default: false },
-//   username: { type: String, requried: true, unique: true },
-//   password: { type: String },
-//   name: { type: String, requried: true },
-//   location: String,
-//   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
-// });
-
-// userSchema.pre("save", async function () {
-//   if (this.isModified("password")) {
-//     this.password = await bcrypt.hash(this.password, 5);
-//   }
-// });
-
-// const User = mongoose.model("User", userSchema);
-// export default User;
