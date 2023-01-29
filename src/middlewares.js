e478232e09597a5aee824a6a0aca35c4c5b1facc
jimkang83 <jim.kang83@gmail.com>
2023-01-29 02:28:18 -0700

    image add2

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,6 +3,7 @@ import aws from "aws-sdk";
 import multerS3 from "multer-s3";
 
 const s3 = new aws.S3({
+  region: "us-west-2",
   credentials: {
     accessKeyId: process.env.AWS_ID,
     secretAccessKey: process.env.AWS_SECRET,
@@ -14,18 +15,23 @@ const isHeroku = process.env.NODE_ENV === "production";
 const s3ImageUploader = multerS3({
   s3: s3,
   bucket: "wetube-clone-2023/images",
-  acl: "public-read",
+  // acl: "public-read",
+  Condition: {
+    StringEquals: {
+      "s3:x-amz-acl": ["public-read"],
+    },
+  },
 });
 
 const s3VideoUploader = multerS3({
   s3: s3,
   bucket: "wetube-clone-2023/videos",
-  acl: "public-read",
-  // Condition: {
-  //   StringEquals: {
-  //     "s3:x-amz-acl": ["public-read"],
-  //   },
-  // },
+  // acl: "public-read",
+  Condition: {
+    StringEquals: {
+      "s3:x-amz-acl": ["public-read"],
+    },
+  },
 });
 
 export const localsMiddleware = (req, res, next) => {
