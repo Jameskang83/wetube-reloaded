dcfeca5213619c24761a3354557e11a39bd51221
jimkang83 <jim.kang83@gmail.com>
2023-01-29 01:22:25 -0700

    test git

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,10 +1,3 @@
-// const hello = async () => {
-//   alert("hi! its working");
-//   const x = await fetch("");
-// };
-
-// hello();
-
 import regeneratorRuntime from "regenerator-runtime";
 import "../scss/styles.scss";
 
